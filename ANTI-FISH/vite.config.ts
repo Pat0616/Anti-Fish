19504208efa1e93866f91701246e36088aa2afcf
Pat0616 <144692539+Pat0616@users.noreply.github.com>
2025-10-21 00:07:07 +0800

    fixed vite config file

--- ANTI-FISH/vite.config.ts
+++ ANTI-FISH/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Anti-Fish/', // 👈 Add this line for GitHub Pages
+  base: '/Anti-Fish/', // Ensure this matches your GitHub repository name
   plugins: [react()],
 })
